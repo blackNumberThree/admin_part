f839085d4022fa2214d930349d801eddc665cdb0
blackNumberThree <46719141+blackNumberThree@users.noreply.github.com>
2020-01-13 01:26:02 +0300

    Add files via upload

---
+++ precache-manifest.c52c950d097a51488d90ac8ece0ea3da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da6ec3783ac6d564ce01e4a819910b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788149e4560f41749399",
+    "url": "/static/css/main.48189c60.chunk.css"
+  },
+  {
+    "revision": "b8373a96634b739aa189",
+    "url": "/static/js/2.d7a0f552.chunk.js"
+  },
+  {
+    "revision": "788149e4560f41749399",
+    "url": "/static/js/main.c27e2832.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
